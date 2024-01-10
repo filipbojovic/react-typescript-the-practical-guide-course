a229d6e87c514f3352d4b210a8ef485f30aec583
Filip Bojovic <fikakv@gmail.com>
2024-01-10 18:03:14 +0100

    Feature: sec04 - Added info box component with dynamic change of its class

--- sec-03-react-typescript-basics/src/components/CourseGoalList.tsx
+++ sec-03-react-typescript-basics/src/components/CourseGoalList.tsx
@@ -1,5 +1,7 @@
+import { ReactNode } from "react";
 import { CourseGoal as CourseGoalType } from "../App";
 import CourseGoal from "./CourseGoal";
+import InfoBox from "./InfoBox";
 
 type CourseGoalListProps = {
   goals: CourseGoalType[];
@@ -7,15 +9,31 @@ type CourseGoalListProps = {
 };
 
 export default function CourseGoalList({ goals, onDeleteGoal }: CourseGoalListProps) {
+  if (goals.length === 0) {
+    return <InfoBox mode="hint">You have no course goals yet. Start adding some!</InfoBox>;
+  }
+
+  let warningBox: ReactNode;
+  if (goals.length >= 4) {
+    warningBox = (
+      <InfoBox mode="warning" severity="high">
+        You're collecting a lot of goals. Don't put too much on your plate!
+      </InfoBox>
+    );
+  }
+
   return (
-    <ul>
-      {goals.map((goal) => (
-        <li key={goal.id}>
-          <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
-            <p>{goal.description}</p>
-          </CourseGoal>
-        </li>
-      ))}
-    </ul>
+    <>
+      {warningBox}
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
+              <p>{goal.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
