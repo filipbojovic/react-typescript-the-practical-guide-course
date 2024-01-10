24a5834ec0a4546397d9e7118c2cd61a0945b233
Filip Bojovic <fikakv@gmail.com>
2024-01-10 09:07:14 +0100

    Feature: Sec-03 032

---
+++ sec-03-react-typescript-basics/src/App.tsx
@@ -0,0 +1,11 @@
+import CourseGoal from "./components/CourseGoal";
+
+export default function App() {
+  return (
+    <main>
+      <CourseGoal title="Learn React + TS">
+        <p>"Learn it"</p>
+      </CourseGoal>
+    </main>
+  );
+}
---
+++ sec-03-react-typescript-basics/src/components/CourseGoal.tsx
@@ -0,0 +1,37 @@
+import { PropsWithChildren } from "react";
+
+// first approach to accept typed props
+// interface CourseGoalProps {
+//   title: string;
+//   children?: ReactNode;
+// }
+
+// the second approach
+type CourseGoalProps = PropsWithChildren<{ title: string }>;
+
+export default function CourseGoal({ title, children }: CourseGoalProps) {
+  return (
+    <article>
+      <div>
+        <h2>{title}</h2>
+        {children}
+      </div>
+      <button>Delete</button>
+    </article>
+  );
+}
+
+// the third approach
+// const CourseGoal: FC<CourseGoalProps> = ({ title, children }) => {
+//   return (
+//     <article>
+//       <div>
+//         <h2>{title}</h2>
+//         {children}
+//       </div>
+//       <button>Delete</button>
+//     </article>
+//   );
+// };
+
+// export default CourseGoal;
